92598ee88246c63213d12efad605bca127a757e0
Teemu Koivisto <tkoivisto456@gmail.com>
2019-03-26 12:51:54 +0200

    WIP change api functionality

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+
+def load_ols_model():
+    with open('models/pickle_model', 'rb') as pmodel:  
+        pickle_model = pickle.load(pmodel)
+        return pickle_model
+
+ols_model = load_ols_model()
 
 featureList = ['Java', 'Pages enabled', 'Issues enabled', 'Scala', 'PHP',
        'Python', 'Default branch', 'Size', 'Contributors Count',
@@ -11,16 +20,18 @@ featureList = ['Java', 'Pages enabled', 'Issues enabled', 'Scala', 'PHP',
        'Pull requests enabled', 'Fork', 'HTML', 'Other', 'CSS', 'Go',
        'Shell', 'Objective-C']
 
-@app.route('/')
-def hello_world():
+@app.route('/api/stars/predict', methods=['GET', 'POST'])
+def ols_predict():
+    body = pd.read_json(request.data, typ='series', dtype=int)
     testx = pd.read_csv('testdata.csv')
     testx = testx[featureList]
-    with open('models/pickle_model', 'rb') as pmodel:  
-        pickle_model = pickle.load(pmodel)
-
-        Ypredict = pickle_model.predict(testx) 
-
-        return Ypredict.to_json()
+    body_cut = body[featureList]
+    body_cut = pd.to_numeric(body_cut)
+    print(testx)
+    print(body_cut)
+    print(len(body_cut))
+    Ypredict = ols_model.predict(body_cut)
+    return jsonify({ "prediction": Ypredict[0] })
 
 if __name__ == "__main__":
     import os
