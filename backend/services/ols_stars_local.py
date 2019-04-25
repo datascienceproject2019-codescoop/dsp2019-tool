31cb3c483bd68e0f82909e3b197d8c4e5e294450
ArktinenSieni <matti.raty@futurice.com>
2019-04-25 17:13:07 +0300

    Local lasso-prediction
    - uses preformatted data and names
    - doesn't use github data
    - old (github data included) is also present when requesting for project's prediction
    - doesn't break?

--- backend/services/ols_stars_local.py
+++ backend/services/ols_stars_local.py
@@ -8,9 +8,14 @@ OLS_MODEL_PATH = 'models/lasso_pickle'
 ENCODER_PATH = 'models/encoder.pickle'
 _ols_model = pickle.load(open(OLS_MODEL_PATH, 'rb'))
 _encoder = pickle.load(open(ENCODER_PATH, 'rb'))
+test_data = None
+preprocessed = None
+prep_names = None
+_hard_coded_sigmay = 807.8974922409773
+_hard_coded_muy = 66.6701413333333
 
 def get_test_data():
-    test_data = None
+    global test_data
 
     if (test_data is None):
         test_data = pd.read_csv('resources/repositories-1.4.0-2018-12-22-rating.csv')
@@ -18,85 +23,38 @@ def get_test_data():
 
     return test_data
 
+  
+def get_prep_data():
+    global preprocessed
+
+    if preprocessed is None:
+        preprocessed = pd.read_csv('resources/pre-processed_datax.csv', usecols=_FEATURES)
+
+    return preprocessed
+
+
+def get_prep_names():
+    global prep_names
+
+    if prep_names is None:
+        prep_names = pd.read_csv('resources/pre-processed_names.csv')
+
+    return prep_names
+
+
 # Hard-coded list of features our model deems are most important
 _FEATURES = [
-  'Language_1',
-  'Deprecated',
-  'License_4',
-  'License_5',
-  'Language_4',
-  'Language_5',
-  'Language_8',
-  'Wiki enabled',
-  'Size',
-  'Language_6',
-  'Language_7',
-  'License_6',
-  'Pull requests enabled',
-  'Default branch',
-  'License_2',
-  'Language_2',
-  'License_3',
-  'Language_3',
-  'Contributors Count',
-  'Fork',
-  'Pages enabled',
-  'Issues enabled',
-  'Open Issues Count',
-  'Watchers Count',
-  'Forks Count'
-  ]
-  
-
-def _get_proper_dict(data: Dict[str, str]) -> Dict[str, List[str]]:
-    """Used to turn dictionaries to form which Pandas understands"""
-    proper = {}
-
-    for k in data.keys():
-      proper[k] = [data[k]]
-
-    return proper
+    'Language_0', 'Language_1', 'Language_2', 'Language_3', 'Language_4',
+    'Language_5', 'Language_6', 'Language_7', 'Language_8', 'License_0',
+    'License_1', 'License_2', 'License_3', 'License_4', 'License_5',
+    'License_6', 'Fork', 'Size', 'Issues enabled', 'Wiki enabled',
+    'Pages enabled', 'Forks Count', 'Open Issues Count', 'Default branch',
+    'Watchers Count', 'Contributors Count', 'Display Name',
+    'Pull requests enabled', 'Deprecated', 'Help Wanted', 'GitHub'
+    ]
 
 
-def _gnumeric_func (data, columns):
-    data[columns] = data[columns].apply(lambda x: pd.factorize(x)[0])
-    return data
-
-
-def _getDummies (data, feature):
-    dummies = pd.get_dummies(data[feature])
-    data = pd.concat([data, dummies], axis=1)
-    data = data.drop([feature],axis=1)
-    return data
-
-
-def _initDatax(data: pd.DataFrame):
-    droplist = ['ID','SourceRank','Description', 'Name with Owner', \
-      'Created Timestamp', "Updated Timestamp", 'Last pushed Timestamp', \
-        'Homepage URL', 'Mirror URL', 'UUID', 'Last Synced Timestamp',\
-          'Fork Source Name with Owner', 'Changelog filename', \
-            'Contributing guidelines filename', 'License filename',\
-              'Code of Conduct filename', 'Security Threat Model filename', \
-                'Security Audit filename', 'SCM type', 'Logo URL', 'Keywords', \
-                  'Stars Count']
-    factorizelist = ['Default branch', 'Display Name', 'Pull requests enabled'] 
-    truefalselist = ['Fork', 'Issues enabled', 'Wiki enabled', 'Pages enabled']
-    dummieslist = ['Status','Host Type']
-    binaryEncodelist = ['Language', 'License']
-
-    datax = data.drop(droplist, axis=1)
-    datax = datax.drop(['Readme filename'], axis=1) #idk why I need to do it separately, needs cleaning
-
-    datax = _gnumeric_func(datax, factorizelist)
-    datax[truefalselist] = datax[truefalselist] * 1
-
-    for item in dummieslist:
-        datax = _getDummies(datax,item)
-        
-    return datax
-
-
-def predict_stars(data_dict) -> np.float64:
+def predict_stars(repo_name: str) -> np.float64:
     """
     Predicts future stars of a Github projects. Regarding input: When using 
     dataframes to call this function, turn them to dictionaries as follows:
@@ -104,17 +62,16 @@ def predict_stars(data_dict) -> np.float64:
     array, when additional call is required as follows: `array[0]`.
     Can throw OSError, if model-file is missing.
     """
-    df = pd.DataFrame.from_dict(_get_proper_dict(data_dict))
-    old_keys = set(df.keys())
-    df = _initDatax(df)
+    names = get_prep_names()['Name with Owner']
+    id = names[names == repo_name].index[0]
 
-    print("((((((((((((((((((one))))))))))))))))))")
-    print(old_keys - set(df.keys()))
-    encoded_df = _encoder.transform(df)
-    print("((((((((((((((((two))))))))))))))))")
-    df_cut = df[_FEATURES]
-    print("(((((((((((((((three)))))))))))))))")
-    prediction = _ols_model.predict(df_cut)
-    print("(((((((((((((((four)))))))))))))))")
+    df = np.array(get_prep_data().iloc[[id]])
+    
+    df = df.reshape(1, -1)
+    prediction = _ols_model.predict(df)[0]
+    denormalized = round((prediction * _hard_coded_sigmay) + _hard_coded_muy)
 
-    return prediction.values[0]
+    if denormalized <= 0:
+        return 0
+
+    return int(denormalized)
